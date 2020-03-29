ee6307c60deb8af4b66cd75344817c2c0c6987fb
Angeli Andrea <and.an@hotmail.com>
2020-03-29 21:30:53 +0200

    - Added alarmState to TrainUser, TrainUserImpl - Implemented Alarm functionality in TrainSensorImpl - Added 4 unit tests under TrainSensorTest

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,48 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainController trainController;
+    TrainUser trainUser;
+    TrainSensor trainSensor;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        trainController = mock(TrainController.class);
+        trainUser = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(trainController, trainUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void OverrideSpeedLimitToNegative_SetsAlarmStateTrue(){
+        trainSensor.overrideSpeedLimit(-1);
+        verify(trainUser).setAlarmState(true);
+    }
+
+    @Test
+    public void OverrideSpeedLimitToOver500_SetsAlarmStateTrue(){
+        trainSensor.overrideSpeedLimit(501);
+        verify(trainUser).setAlarmState(true);
+    }
+
+    @Test
+    public void OverrideSpeedLimitToLessThan50Percent_SetsAlarmStateTrue(){
+        when(trainController.getReferenceSpeed()).thenReturn(50);
+        trainSensor.overrideSpeedLimit(24);
+        verify(trainUser).setAlarmState(true);
+    }
+
+    @Test
+    public void OverrideSpeedLimitToCorrectValue_SetsAlarmStateFalse(){
+        when(trainController.getReferenceSpeed()).thenReturn(50);
+        trainSensor.overrideSpeedLimit(26);
+        verify(trainUser).setAlarmState(false);
     }
 }
